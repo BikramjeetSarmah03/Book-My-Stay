b953417e80b25dd84d8f0923a2e42fad0949b8dc
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2024-04-21 19:48:13 +0530

    UI: Profile Left

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { listData } from '../properties/properties.component';
+import { PropertyCardComponent } from '../../components/cards/property-card/property-card.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [PropertyCardComponent],
   templateUrl: './profile.component.html',
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  listData = listData;
+}
