ae4e970a9137c96b8614798f7ddafde7aabc0339
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2024-04-21 11:33:42 +0530

    UI: Slider

--- src/app/components/common/slider/slider.component.ts
+++ src/app/components/common/slider/slider.component.ts
@@ -8,5 +8,31 @@ import { Component, Input } from '@angular/core';
   styleUrl: './slider.component.css',
 })
 export class SliderComponent {
-  @Input() images!: any;
+  @Input() images!: any[];
+
+  imageIndex: number | null = null;
+
+  changeSlide(direction: 'left' | 'right') {
+    if (direction === 'left') {
+      if (this.imageIndex === 0 || this.imageIndex === null) {
+        this.imageIndex = this.images.length - 1;
+      } else {
+        this.imageIndex = this.imageIndex - 1;
+      }
+    } else {
+      if (
+        this.imageIndex === this.images.length - 1 ||
+        this.imageIndex === null
+      ) {
+        this.imageIndex = 0;
+      } else {
+        this.imageIndex = this.imageIndex + 1;
+      }
+    }
+  }
+
+  resetIndex(idx: number | null) {
+    this.imageIndex = idx;
+    console.log(this.imageIndex);
+  }
 }
